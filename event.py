d6d9373d461f5aac0b1ab6d8ecaec9273f9de67e
zhitm <64278750+zhitm@users.noreply.github.com>
2020-09-21 23:13:56 +0300

    gitignore

--- event.py
+++ event.py
@@ -9,6 +9,7 @@ class Event:
 		hour = localtime().tm_mday*24 + localtime().tm_hour
 		min = localtime().tm_min
 		sec = localtime().tm_sec
-		self.timer = ((self.time['day']*24 + self.time['hour']-hour)*60 + self.time['minute'] - min) * 60  - sec
+		self.timer = ((self.time['day']*24 + self.time['hour']-hour)*60 + self.time['minute'] - min) * 60  - sec #я потом буду учитывать год и месяц, мне лень
+		
 
 
